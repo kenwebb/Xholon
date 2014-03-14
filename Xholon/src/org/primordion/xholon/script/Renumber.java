229ec9139c14d4152e8f8d53ba5ae4b1fc61c885
kenwebb <ken@primordion.com>
2014-03-14 15:15:23 -0400

    allow optional script attribute that sets root nextId

--- Xholon/src/org/primordion/xholon/script/Renumber.java
+++ Xholon/src/org/primordion/xholon/script/Renumber.java
@@ -22,30 +22,40 @@ import org.primordion.xholon.base.IXholon;
 
 /**
  * This Xholon script renumbers all nodes in the subtree.
- * It reuses the current ID of it's parent node as the starting point.
- * This may not always be the best way to renumber.
+ * It uses a supplied value, or reuses the current ID of it's parent node, as the starting point.
  * <p>This script is located in the common default package for Java scripts.
  * It can therefore be pasted into a running Xholon app simply as:</p>
  * <pre>&lt;Renumber/></pre>
+ * <pre>&lt;Renumber nextId="1"/></pre>
  * @author <a href="mailto:ken@primordion.com">Ken Webb</a>
  * @see <a href="http://www.primordion.com/Xholon">Xholon Project website</a>
  * @since 0.8.1 (Created on February 16, 2010)
  */
 public class Renumber extends XholonScript {
-	
-	private int nextId = 0;
-	
-	public void postConfigure()
-	{
-		nextId = getParentNode().getId();
-		getParentNode().visit(this);
-        removeChild();
-	}
-	
-    public boolean visit(IXholon visitee)
-    {
-    	visitee.setId(nextId++);
-    	return true;
+  
+  private int nextId = -1;
+  
+  public void postConfigure() {
+    if (nextId == -1) {
+      nextId = getParentNode().getId();
     }
-    
+    getParentNode().visit(this);
+    if (nextSibling != null) {
+      nextSibling.postConfigure();
+    }
+    removeChild();
+  }
+  
+  public boolean visit(IXholon visitee) {
+    visitee.setId(nextId++);
+    return true;
+  }
+  
+  public int setAttributeVal(String attrName, String attrVal) {
+    if ("nextId".equals(attrName)) {
+      nextId = Integer.parseInt(attrVal);
+    }
+    return 0;
+  }
+  
 }
