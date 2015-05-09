a2631baddb705eba4dd63880c9e912655e866ab5
kenwebb <ken@primordion.com>
2015-05-09 10:07:48 -0400

    fixed bug that showed up in Wolf Sheep Grass app

--- Xholon/src/org/primordion/xholon/base/PatchOwner.java
+++ Xholon/src/org/primordion/xholon/base/PatchOwner.java
@@ -48,9 +48,6 @@ public class PatchOwner extends AbstractGrid implements IXholon {
 	protected int nextColor = 135;
 	protected double nextHeading = 345.0;
 	
-	// prevent needless recursive calls on act() for all patches and turtles
-	public void act() {}
-	
 	/**
 	 * Get next color.
 	 * @return A color value.
