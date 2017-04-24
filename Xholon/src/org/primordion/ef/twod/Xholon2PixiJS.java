77be3f16a3b982909ae5531e19cacaa568f10530
Ken Webb <ken@primordion.com>
2017-04-24 14:13:52 -0400

    Xholon2PixiJS various initial edits

--- Xholon/src/org/primordion/ef/twod/Xholon2PixiJS.java
+++ Xholon/src/org/primordion/ef/twod/Xholon2PixiJS.java
@@ -23,12 +23,10 @@ import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.Image;
 
 import java.util.Date;
-//import java.util.List;
 
 import org.client.HtmlElementCache;
 import org.primordion.xholon.base.IDecoration;
 import org.primordion.xholon.base.IXholon;
-//import org.primordion.xholon.base.PortInformation;
 import org.primordion.xholon.common.mechanism.CeStateMachineEntity;
 import org.primordion.ef.AbstractXholon2ExternalFormat;
 import org.primordion.xholon.io.gwt.HtmlScriptHelper;
@@ -108,13 +106,26 @@ public class Xholon2PixiJS extends AbstractXholon2ExternalFormat implements IXho
     }
   }
   
+  /**
+   * KLayJson layouter
+   */
   protected native void layoutKLayJson(IXholon root, int width, int height) /*-{
     $wnd.xh.xport("_other,KLayJson", root,
-     '{"nameTemplate":"R^^^^^", "showEdges":false, "showPorts":false, "direction":"UNDEFINED", "edgeRouting":"ORTHOGONAL", "spacing":20, "width":'
+     '{"nameTemplate":"R^^^^^", "showEdges":true, "showPorts":true, "direction":"UNDEFINED", "edgeRouting":"ORTHOGONAL", "spacing":'
+     + this.efParams.spacing
+     + ', "width":'
      + width
      + ', "height":'
      + height
-     + ', "nodeWidth":20, "nodeHeight":20, "portWidth":5, "portHeight":5, "selection":"#xhsvg", "maxChars":3, "writeToTarget":true, "writeLayoutedToTarget":true, "display":false, "_jsdata":true, "showStateMachineEntities":false}');
+     + ', "nodeWidth":'
+     + this.efParams.nodeWidth
+     + ', "nodeHeight":'
+     + this.efParams.nodeHeight
+     + ', "portWidth":5, "portHeight":5, "selection":"#xhsvg", "maxChars":'
+     + this.efParams.maxChars
+     + ', "writeToTarget":true, "writeLayoutedToTarget":true, "display":false, "_jsdata":true, "showStateMachineEntities":'
+     + this.efParams.shouldShowStateMachineEntities
+     + '}');
   }-*/;
   
   @Override
@@ -129,7 +140,7 @@ public class Xholon2PixiJS extends AbstractXholon2ExternalFormat implements IXho
     String[] jsdata = getJsdata(xhNode).split(",");
     sb
     .append("var ").append(xhNodeName).append(" = new $wnd.PIXI.Graphics();\n")
-    .append(xhNodeName).append(".beginFill(").append(((IDecoration)xhNode).getColor()).append(" || 0x808080);\n")
+    .append(xhNodeName).append(".beginFill(").append(((IDecoration)xhNode).getColor()).append(" || 0xF0F8FF, 0.1);\n")
     .append(xhNodeName).append(".drawRect(0,0,").append(jsdata[1]).append(",").append(jsdata[0]).append(");\n")
     .append(xhNodeName).append(".endFill();\n")
     .append(xhNodeName).append(".x = ").append(jsdata[2]).append(";\n")
@@ -178,6 +189,7 @@ public class Xholon2PixiJS extends AbstractXholon2ExternalFormat implements IXho
     .append("var $wnd = window;\n")
     .append("var $doc = document;\n")
     .append("var renderer = $wnd.PIXI.autoDetectRenderer(").append(this.getWidth()).append(", ").append(this.getHeight()).append(");\n")
+    //.append("renderer.backgroundColor = 0xFFFFFF;\n")
     .append("$doc.querySelector(\"div#xhcanvas\").appendChild(renderer.view);\n")
     .append("//Create a container object called the `stage`. The `stage` is the root container for all your objects.\n")
     .append("var stage = new $wnd.PIXI.Container();\n");
@@ -215,153 +227,141 @@ public class Xholon2PixiJS extends AbstractXholon2ExternalFormat implements IXho
     var p = {};
     p.shouldShowStateMachineEntities = false;
     p.nameTemplate = "^^C^^^";
-    p.xhAttrReturnAll = true;
-    p.writeXholonId = false;
-    p.writeXholonRoleName = true;
-    p.writePorts = false;
-    p.writeAnnotations = true;
-    p.shouldPrettyPrint = true;
-    p.writeAttributes = true;
-    p.writeStandardAttributes = true;
-    p.shouldWriteVal = true;
-    p.shouldWriteAllPorts = true;
+    //p.xhAttrReturnAll = true;
+    //p.writeXholonId = false;
+    //p.writeXholonRoleName = true;
+    //p.writePorts = false;
+    //p.writeAnnotations = true;
+    //p.shouldPrettyPrint = true;
+    //p.writeAttributes = true;
+    //p.writeStandardAttributes = true;
+    //p.shouldWriteVal = true;
+    //p.shouldWriteAllPorts = true;
     p.layouter = "KLayJson"; // "Graphviz", "" or "None"
     p.width = 600;
     p.height = 600;
+    // KLayJson params
+    p.spacing = 20; // 20 is the KLay default
+    p.nodeWidth = 20;
+    p.nodeHeight = 20;
+    p.maxChars = 1;
     this.efParams = p;
   }-*/;
 
   public native boolean isShouldShowStateMachineEntities() /*-{
-    return efParams.shouldShowStateMachineEntities;
+    return this.efParams.shouldShowStateMachineEntities;
   }-*/;
-
-  public native void setShouldShowStateMachineEntities(
-      boolean shouldShowStateMachineEntities) /*-{
-    this.efParams.shouldShowStateMachineEntities = shouldShowStateMachineEntities;
-  }-*/;
+  //public native void setShouldShowStateMachineEntities(boolean shouldShowStateMachineEntities) /*-{
+  //  this.efParams.shouldShowStateMachineEntities = shouldShowStateMachineEntities;
+  //}-*/;
   
-  public native String getNameTemplate() /*-{
-    return this.efParams.nameTemplate;
-  }-*/;
-
-  public native void setNameTemplate(String nameTemplate) /*-{
-    this.efParams.nameTemplate = nameTemplate;
-  }-*/;
+  public native String getNameTemplate() /*-{return this.efParams.nameTemplate;}-*/;
+  //public native void setNameTemplate(String nameTemplate) /*-{this.efParams.nameTemplate = nameTemplate;}-*/;
 
   /*
    * @see org.primordion.xholon.io.xml.IXholon2Xml#getXhAttrReturnAll()
    */
-  public native boolean getXhAttrReturnAll() /*-{
-    return this.efParams.xhAttrReturnAll;
-  }-*/;
-  
+  //public native boolean getXhAttrReturnAll() /*-{
+  //  return this.efParams.xhAttrReturnAll;
+  //}-*/;
   /*
    * @see org.primordion.xholon.io.xml.IXholon2Xml#setXhAttrReturnAll(boolean)
    */
-  public native void setXhAttrReturnAll(boolean xhAttrReturnAll) /*-{
-    this.efParams.xhAttrReturnAll = xhAttrReturnAll;
-  }-*/;
+  //public native void setXhAttrReturnAll(boolean xhAttrReturnAll) /*-{
+  //  this.efParams.xhAttrReturnAll = xhAttrReturnAll;
+  //}-*/;
   
   /* 
    * @see org.primordion.xholon.io.xml.IXholon2Xml#isWriteXholonId()
    */
-  public native boolean isWriteXholonId() /*-{
-    return this.efParams.writeXholonId;
-  }-*/;
-  
+  //public native boolean isWriteXholonId() /*-{
+  //  return this.efParams.writeXholonId;
+  //}-*/;
   /* 
    * @see org.primordion.xholon.io.xml.IXholon2Xml#setWriteXholonId(boolean)
    */
-  public native void setWriteXholonId(boolean writeXholonId) /*-{
-    this.efParams.writeXholonId = writeXholonId;
-  }-*/;
+  //public native void setWriteXholonId(boolean writeXholonId) /*-{
+  //  this.efParams.writeXholonId = writeXholonId;
+  //}-*/;
   
   /*
    * @see org.primordion.xholon.io.xml.IXholon2Xml#isWriteXholonRoleName()
    */
-  public native boolean isWriteXholonRoleName() /*-{
-    return this.efParams.writeXholonRoleName;
-  }-*/;
-
+  //public native boolean isWriteXholonRoleName() /*-{
+  //  return this.efParams.writeXholonRoleName;
+  //}-*/;
   /*
    * @see org.primordion.xholon.io.xml.IXholon2Xml#setWriteXholonRoleName(boolean)
    */
-  public native void setWriteXholonRoleName(boolean writeXholonRoleName) /*-{
-    this.efParams.writeXholonRoleName = writeXholonRoleName;
-  }-*/;
+  //public native void setWriteXholonRoleName(boolean writeXholonRoleName) /*-{
+  //  this.efParams.writeXholonRoleName = writeXholonRoleName;
+  //}-*/;
 
   /*
    * @see org.primordion.xholon.io.xml.IXholon2Xml#isWritePorts()
    */
-  public native boolean isWritePorts() /*-{
-    return this.efParams.writePorts;
-  }-*/;
-  
+  //public native boolean isWritePorts() /*-{
+  //  return this.efParams.writePorts;
+  //}-*/;
   /*
    * @see org.primordion.xholon.io.xml.IXholon2Xml#setWritePorts(boolean)
    */
-  public native void setWritePorts(boolean writePorts) /*-{
-    this.efParams.writePorts = writePorts;
-  }-*/;
+  //public native void setWritePorts(boolean writePorts) /*-{
+  //  this.efParams.writePorts = writePorts;
+  //}-*/;
 
-  public native boolean isShouldPrettyPrint() /*-{
-    return this.efParams.shouldPrettyPrint;
-  }-*/;
-
-  public native void setShouldPrettyPrint(boolean shouldPrettyPrint) /*-{
-    this.efParams.shouldPrettyPrint = shouldPrettyPrint;
-  }-*/;
+  //public native boolean isShouldPrettyPrint() /*-{
+  //  return this.efParams.shouldPrettyPrint;
+  //}-*/;
+  //public native void setShouldPrettyPrint(boolean shouldPrettyPrint) /*-{
+  //  this.efParams.shouldPrettyPrint = shouldPrettyPrint;
+  //}-*/;
   
   /*
    * @see org.primordion.xholon.io.xml.IXholon2Xml#isWriteAttributes()
    */
-  public native boolean isWriteAttributes() /*-{
-    return this.efParams.writeAttributes;
-  }-*/;
-
+  //public native boolean isWriteAttributes() /*-{
+  //  return this.efParams.writeAttributes;
+  //}-*/;
   /*
    * @see org.primordion.xholon.io.xml.IXholon2Xml#setWriteAttributes(boolean)
    */
-  public native void setWriteAttributes(boolean writeAttributes) /*-{
-    this.efParams.writeAttributes = writeAttributes;
-  }-*/;
+  //public native void setWriteAttributes(boolean writeAttributes) /*-{
+  //  this.efParams.writeAttributes = writeAttributes;
+  //}-*/;
 
   /*
    * @see org.primordion.xholon.io.xml.IXholon2Xml#isWriteStandardAttributes()
    */
-  public native boolean isWriteStandardAttributes() /*-{
-    return this.efParams.writeStandardAttributes;
-  }-*/;
-
+  //public native boolean isWriteStandardAttributes() /*-{
+  //  return this.efParams.writeStandardAttributes;
+  //}-*/;
   /*
    * @see org.primordion.xholon.io.xml.IXholon2Xml#setWriteStandardAttributes(boolean)
    */
-  public native void setWriteStandardAttributes(boolean writeStandardAttributes) /*-{
-    this.efParams.writeStandardAttributes = writeStandardAttributes;
-  }-*/;
+  //public native void setWriteStandardAttributes(boolean writeStandardAttributes) /*-{
+  //  this.efParams.writeStandardAttributes = writeStandardAttributes;
+  //}-*/;
   
-  public native String getLayouter() /*-{
-    return this.efParams.layouter;
-  }-*/;
-
-  public native void setLayouter(String layouter) /*-{
-    this.efParams.layouter = layouter;
-  }-*/;
+  public native String getLayouter() /*-{return this.efParams.layouter;}-*/;
+  //public native void setLayouter(String layouter) /*-{this.efParams.layouter = layouter;}-*/;
   
-  public native int getWidth() /*-{
-    return this.efParams.width;
-  }-*/;
+  public native int getWidth() /*-{return this.efParams.width;}-*/;
+  //public native void setWidth(int width) /*-{this.efParams.width = width;}-*/;
   
-  public native void setWidth(int width) /*-{
-    this.efParams.width = width;
-  }-*/;
+  public native int getHeight() /*-{return this.efParams.height;}-*/;
+  //public native void setHeight(int height) /*-{this.efParams.height = height;}-*/;
   
-  public native int getHeight() /*-{
-    return this.efParams.height;
-  }-*/;
+  public native int getSpacing() /*-{return this.efParams.spacing;}-*/;
+  //public native void setSpacing(int spacing) /*-{this.efParams.spacing = spacing;}-*/;
   
-  public native void setHeight(int height) /*-{
-    this.efParams.height = height;
-  }-*/;
+  public native int getNodeWidth() /*-{return this.efParams.nodeWidth;}-*/;
+  //public native void setNodeWidth(int nodeWidth) /*-{this.efParams.nodeWidth = nodeWidth;}-*/;
+  
+  public native int getNodeHeight() /*-{return this.efParams.nodeHeight;}-*/;
+  //public native void setNodeHeight(int nodeHeight) /*-{this.efParams.nodeHeight = nodeHeight;}-*/;
+  
+  public native int getMaxChars() /*-{return this.efParams.maxChars;}-*/;
+  //public native void setMaxChars(int maxChars) /*-{this.efParams.maxChars = maxChars;}-*/;
   
 }
