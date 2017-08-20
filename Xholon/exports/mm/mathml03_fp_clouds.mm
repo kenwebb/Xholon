1c0469754e4c4c1a22589535c146d49ae850ca0c
Ken Webb <ken@primordion.com>
2017-08-20 05:47:18 -0400

    new MindMap exports

---
+++ Xholon/exports/mm/mathml03_fp_clouds.mm
@@ -0,0 +1,116 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="MathML_Test1" ID="0" CREATED="1502634074935" MODIFIED="1502635737063"><hook NAME="MapStyle" zoom="1.5982759">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="math 154.1" POSITION="right" ID="1" CREATED="1502634074935" MODIFIED="1502635107584">
+<node TEXT="apply" ID="2" CREATED="1502634074935" MODIFIED="1502635801125">
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<node TEXT="plus" ID="3" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="5.1" ID="4" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="2" ID="5" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="3" ID="6" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="apply" ID="7" CREATED="1502634074935" MODIFIED="1502635107584">
+<node TEXT="times" ID="8" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="8" ID="9" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="9" ID="10" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="apply" ID="11" CREATED="1502634074935" MODIFIED="1502635107584">
+<node TEXT="abs" ID="12" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="-2" ID="13" CREATED="1502634074935" MODIFIED="1502635107584"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="math 120" POSITION="right" ID="14" CREATED="1502634074935" MODIFIED="1502635107584">
+<node TEXT="apply" ID="15" CREATED="1502634074935" MODIFIED="1502635862300">
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<node TEXT="factorial" ID="16" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="5" ID="17" CREATED="1502634074935" MODIFIED="1502635107584"/>
+</node>
+</node>
+<node TEXT="math 4" POSITION="right" ID="18" CREATED="1502634074935" MODIFIED="1502635107584">
+<node TEXT="apply" ID="19" CREATED="1502634074935" MODIFIED="1502635894763">
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<node TEXT="minus" ID="20" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="7" ID="21" CREATED="1502634074935" MODIFIED="1502635107584"/>
+<node TEXT="3" ID="22" CREATED="1502634074935" MODIFIED="1502635107584"/>
+</node>
+</node>
+<node TEXT="math 2.6" POSITION="right" ID="23" CREATED="1502634074935" MODIFIED="1502635107585">
+<node TEXT="apply" ID="24" CREATED="1502634074935" MODIFIED="1502635909129">
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<node TEXT="divide" ID="25" CREATED="1502634074935" MODIFIED="1502635107585"/>
+<node TEXT="13" ID="26" CREATED="1502634074935" MODIFIED="1502635107585"/>
+<node TEXT="5" ID="27" CREATED="1502634074935" MODIFIED="1502635107585"/>
+</node>
+</node>
+<node TEXT="math 8" POSITION="right" ID="28" CREATED="1502634074935" MODIFIED="1502635107585">
+<node TEXT="apply" ID="29" CREATED="1502634074935" MODIFIED="1502635922681">
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<node TEXT="power" ID="30" CREATED="1502634074935" MODIFIED="1502635107585"/>
+<node TEXT="2" ID="31" CREATED="1502634074935" MODIFIED="1502635107585"/>
+<node TEXT="3" ID="32" CREATED="1502634074935" MODIFIED="1502635107585"/>
+</node>
+</node>
+<node TEXT="TestCi" POSITION="right" ID="33" CREATED="1502634074935" MODIFIED="1502635107585">
+<node TEXT="math 666" ID="34" CREATED="1502634074935" MODIFIED="1502635107585">
+<node TEXT="apply" ID="35" CREATED="1502634074935" MODIFIED="1502635945850">
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<node TEXT="plus" ID="36" CREATED="1502634074935" MODIFIED="1502635107585"/>
+<node TEXT="153" ID="37" CREATED="1502634074935" MODIFIED="1502635107585">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#6666ff" WIDTH="2" TRANSPARENCY="255" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="33" STARTINCLINATION="192;0;" ENDINCLINATION="40;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="513" ID="38" CREATED="1502634074935" MODIFIED="1502635107585">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#6666ff" WIDTH="2" TRANSPARENCY="255" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="33" STARTINCLINATION="192;0;" ENDINCLINATION="40;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
