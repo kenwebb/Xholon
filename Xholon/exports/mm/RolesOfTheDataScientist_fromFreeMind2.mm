9e11216d91d8e80b219c55023239f5f5397f8ef4
Ken Webb <ken@primordion.com>
2018-04-02 07:05:31 -0400

    new exports

---
+++ Xholon/exports/mm/RolesOfTheDataScientist_fromFreeMind2.mm
@@ -0,0 +1,139 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1517752303428" ID="70" MODIFIED="1517752780580" TEXT="Areas of Expertise">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1517752303428" ID="71" MODIFIED="1517752780528" POSITION="right" TEXT="Problem Domain">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1517752303428" ID="72" MODIFIED="1517752780529" POSITION="right" TEXT="Data Collection">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1517752303428" ID="73" MODIFIED="1517752780532" TEXT="API Wrangling">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="74" MODIFIED="1517752780535" TEXT="Web Scraping">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="75" MODIFIED="1517752780537" TEXT="Questionnaire Design + Use">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="76" MODIFIED="1517752780538" TEXT="Sensor Design">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1517752303428" ID="77" MODIFIED="1517752780540" POSITION="right" TEXT="Data Storage">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1517752303428" ID="78" MODIFIED="1517752780541" TEXT="Database Design">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="79" MODIFIED="1517752780543" TEXT="Streaming Data">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="80" MODIFIED="1517752780545" TEXT="Big Data Wrangling">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="81" MODIFIED="1517752780547" TEXT="Data Querying">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="82" MODIFIED="1517752780548" TEXT="Database Implementation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1517752303428" ID="83" MODIFIED="1517752780550" POSITION="right" TEXT="Data Preparation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1517752303428" ID="84" MODIFIED="1517752780552" TEXT="Evaluating">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="85" MODIFIED="1517752780554" TEXT="Cleaning">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="86" MODIFIED="1517752780555" TEXT="Restructuring">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="87" MODIFIED="1517752780557" TEXT="Condensing">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="88" MODIFIED="1517752780558" TEXT="Transforming">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1517752303428" ID="89" MODIFIED="1517752780560" POSITION="right" TEXT="Data Analysis">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1517752303428" ID="90" MODIFIED="1517752780562" TEXT="Classification">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="91" MODIFIED="1517752780563" TEXT="Clustering">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="92" MODIFIED="1517752780565" TEXT="Conceptual Analysis">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="93" MODIFIED="1517752780566" TEXT="Value Estimation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="94" MODIFIED="1517752780567" TEXT="Anomaly Detection">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="95" MODIFIED="1517752780569" TEXT="Algorithm Performance">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="96" MODIFIED="1517752780571" TEXT="Text Mining">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="97" MODIFIED="1517752780572" TEXT="Sentiment Analysis">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1517752303428" ID="98" MODIFIED="1517752780574" POSITION="right" TEXT="Data Presentation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1517752303428" ID="99" MODIFIED="1517752780575" TEXT="Web Apps">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="100" MODIFIED="1517752780576" TEXT="Dashboard Design">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="101" MODIFIED="1517752780578" TEXT="Data Visualization">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="102" MODIFIED="1517752780579" TEXT="UX">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1517752303428" ID="103" MODIFIED="1517752780580" TEXT="Infographics">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
