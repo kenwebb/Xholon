68321bb516542bb174a8c0e4d78c0442e35fe48e
Ken Webb <ken@primordion.com>
2018-10-02 10:25:06 -0400

    commented out unreachable code

--- Xholon/src/org/primordion/ef/d3/Xholon2ForceDirected.java
+++ Xholon/src/org/primordion/ef/d3/Xholon2ForceDirected.java
@@ -67,53 +67,53 @@ public class Xholon2ForceDirected extends AbstractXholon2ExternalFormat implemen
 	  $wnd.alert("D3 Force-Directed Diagram not yet implemented");
 	  return;
 	  
-	  var width = 960,
-    height = 500;
+//	  var width = 960,
+//    height = 500;
 
-    var color = d3.scale.category20();
+//    var color = d3.scale.category20();
 
-    var force = d3.layout.force()
-        .charge(-120)
-        .linkDistance(30)
-        .size([width, height]);
+//    var force = d3.layout.force()
+//        .charge(-120)
+//        .linkDistance(30)
+//        .size([width, height]);
 
-    var svg = d3.select(selection).append("svg")
-        .attr("width", width)
-        .attr("height", height);
+//    var svg = d3.select(selection).append("svg")
+//       .attr("width", width)
+//        .attr("height", height);
 
-    //d3.json("miserables.json", function(error, graph) {
-      force
-          .nodes(json.nodes)
-          .links(json.links)
-          .start();
+//    //d3.json("miserables.json", function(error, graph) {
+//      force
+//          .nodes(json.nodes)
+//          .links(json.links)
+//          .start();
 
-      var link = svg.selectAll(".link")
-          .data(json.links)
-        .enter().append("line")
-          .attr("class", "link")
-          .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+//      var link = svg.selectAll(".link")
+//          .data(json.links)
+//        .enter().append("line")
+//          .attr("class", "link")
+//          .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-      var node = svg.selectAll(".node")
-          .data(json.nodes)
-        .enter().append("circle")
-          .attr("class", "node")
-          .attr("r", 5)
-          .style("fill", function(d) { return color(d.group); })
-          .call(force.drag);
+//      var node = svg.selectAll(".node")
+//          .data(json.nodes)
+//        .enter().append("circle")
+//          .attr("class", "node")
+//          .attr("r", 5)
+//          .style("fill", function(d) { return color(d.group); })
+//          .call(force.drag);
 
-      node.append("title")
-          .text(function(d) { return d.name; });
+//      node.append("title")
+//          .text(function(d) { return d.name; });
 
-      force.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+//      force.on("tick", function() {
+//        link.attr("x1", function(d) { return d.source.x; })
+//            .attr("y1", function(d) { return d.source.y; })
+//            .attr("x2", function(d) { return d.target.x; })
+//            .attr("y2", function(d) { return d.target.y; });
 
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
-      });
-    //});
+//        node.attr("cx", function(d) { return d.x; })
+//            .attr("cy", function(d) { return d.y; });
+//      });
+//    //});
 	}-*/;
 	
 }
