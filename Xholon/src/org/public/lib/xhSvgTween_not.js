bfd581d97df5ada01f674b101e2ae4161e377b93
Ken Webb <ken@primordion.com>
2018-11-21 16:57:16 -0500

    version of xhSvgTween.js that does nothing

---
+++ Xholon/src/org/public/lib/xhSvgTween_not.js
@@ -0,0 +1,18 @@
+// Dummy replacement for transitioning between 2 or more adjacent SVG images.
+// xhSvgTween_not.js
+// Ken Webb  November 8, 2018
+// MIT License, Copyright (C) 2018 Ken Webb
+// Designed to NOT tween between 2 D3-CirclePack SVG images.
+
+// usage: xh.tween();
+//        xh.tween("#xhgraph", 5);
+//        xh.tween(null,2);
+
+if (typeof xh == "undefined") {
+  xh = {};
+}
+
+xh.tween = function(selection, duration, sortedArr1) {
+// I am just a dummy. I do nothing.
+}
+
