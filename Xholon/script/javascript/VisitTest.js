3f128806743fea31d45605a907eb0c4d263f72cc
Ken Webb <ken@primordion.com>
2020-03-14 04:26:47 -0400

    enhancements to VisitTest.js

--- Xholon/script/javascript/VisitTest.js
+++ Xholon/script/javascript/VisitTest.js
@@ -11,16 +11,21 @@
   The Visitor pattern can operate as a form of map-reduce.
   In this example, "count" is a reduce result.
   I could also map to an array of node names, or IDs, or (in this example) states.
+  Haskell has fold functions; Ramda has reduce functions.
 */
 
-var vbeh = `<XingHelloWorldSystembehavior implName="org.primordion.xholon.base.Behavior_gwtjs"><![CDATA[
+var vbeh = `<VisitTest implName="org.primordion.xholon.base.Behavior_gwtjs"><![CDATA[
 var me, count, arr, testing;
 var beh = {
   postConfigure: function() {
     me = this.cnode.parent();
     testing = Math.floor(Math.random() * 10);
     count = 0;
+    me.length = 0; // makes me somewhat array-like
     arr = [];
+    
+    //me.visit(this.cnode);
+    //this.cnode.remove();
   },
   act: function() {
     me.println(this.toString());
@@ -28,11 +33,15 @@ var beh = {
   visit: function(visitor) {
     me.println("visited by " + visitor);
     this.doReduceCount();
+    this.doReduceLength();
     this.doMapArrIXholon(visitor);
     return true;
   },
   doReduceCount: function() {
     count++;
+  },
+  doReduceLength: function() {
+    me.length++;
   },
   doMapArrIXholon: function(visitor) {
     arr.push(visitor.name());
@@ -42,8 +51,8 @@ var beh = {
     return "testing:" + testing + " count:" + count + " arr:" + arr;
   }
 }
-//# sourceURL=behaviorScript.js
-]]></XingHelloWorldSystembehavior>`;
+//# sourceURL=VisitTest.js
+]]></VisitTest>`;
 
 var node = xh.root();
 var visitee = node.append(vbeh).last();
@@ -55,4 +64,13 @@ result in out tab:
 visited by helloWorldSystem_0 state=0
 visited by hello_2 [ port:world_3] state=0
 visited by world_3 [ port:hello_2] state=0
+
+result of calling step() for Cell model (note that the arr is serialized, naturally ordered by id):
+testing:6 count:29 arr:extraCellularSpace_0,extraCellularSolution_1,glucose_2,eukaryoticCell_3,cellMembrane_4,
+cellBilayer_5,cytoplasm_6,cytosol_7,glucose_8,glucose_6_Phosphate_9,fructose_6_Phosphate_10,fructose_1x6_Biphosphate_11,
+dihydroxyacetonePhosphate_12,glyceraldehyde_3_Phosphate_13,x1x3_BisphosphoGlycerate_14,x3_PhosphoGlycerate_15,
+x2_PhosphoGlycerate_16,phosphoEnolPyruvate_17,pyruvate_18,hexokinase_19,phosphoGlucoIsomerase_20,phosphoFructokinase_21,
+aldolase_22,triosePhosphateIsomerase_23,glyceraldehyde_3_phosphateDehydrogenase_24,phosphoGlycerokinase_25,
+phosphoGlyceromutase_26,enolase_27,pyruvateKinase_28
+
 */
