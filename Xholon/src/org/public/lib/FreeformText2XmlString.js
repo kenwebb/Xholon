82b6caf793642f001becf587d856c6813feb24f9
Ken Webb <ken@primordion.com>
2020-06-13 15:07:25 -0400

    FreeformText2XmlString early development

--- Xholon/src/org/public/lib/FreeformText2XmlString.js
+++ Xholon/src/org/public/lib/FreeformText2XmlString.js
@@ -13,8 +13,50 @@
  * Usage:
  * http://127.0.0.1:8080/war/Xholon.html?app=APPNAME&src=lstr&gui=clsc&jslib=FreeformText2XmlString
  * http://127.0.0.1:8080/war/XholonInteract.html?app=6901ef51d5fcb2d86a21&src=gist&gui=none&jslib=FreeformText2XmlString
+ * http://127.0.0.1:8888/Xholon.html?app=6ee032b8913680e72191ffea48dca46f&src=gist&gui=clsc&jslib=nearley/nearley,nearley/fipaAcl,fipa,FreeformText2XmlString
  * 
  * see also: my notes in my paper notebook from June 11/12 2020
+ * 
+ * Examples for testing:
+One
+Two deux
+Three trois quatre
+
+ eins
+ testing
+ 
+ a b c
+ 
+NO <Hello><Role action="replace">howdy</Role></Hello>
+
+<Hello><Role roleName="howdy" action="replace"></Role></Hello>
+<Hello><Role roleName="howdy" action="replace"/></Hello>
+
+NO <Hello><Anno>howdy</Anno></Hello>
+<Hello><Annotation>Mercury Venus</Annotation></Hello>
+
+Four
+Five
+Six
+Ten
+
+<Role roleName="june13" action="replace"></Role>
+<Role roleName="june13" action="replace"/>
+<Role roleName="kjune13" action="replace"></Role>
+
+References
+Notes
+ *
+ * TODO
+- DONE if the input String is a single word, and if it starts with "http", then it should be treated as an Annotation
+- optionally append new text to an existing annotation, where the new text will be preceded by a newline
+- Xholon workbook:
+ - provide a Xholon subtree that includes nodes for workbook sections: Notes References IH CD CSH etc.
+ - maybe create a new Xholon mechanism with node types that can properly write-out parts of a workbook
+- optionally add new XholonClass names to the IH as well as to the CSH
+- use default child names:
+ - ex: if I drag "Licorice" into a Cats node, the result should be <Cat>Licorice</Cat>
+ * 
  */
 
 if (typeof xh == "undefined") {
@@ -23,30 +65,38 @@ if (typeof xh == "undefined") {
 
 xh.fftxt2xmlstr = {}
 
+xh.fftxt2xmlstr.defaultParams = {
+  roleAction: "replace"
+}
+
 // (JSON, String) -> XMLString
-xh.fftxt2xmlstr.transform = (params, fftxt) => {
+xh.fftxt2xmlstr.transform = (argParams, fftxt) => {
+  const params = argParams || xh.fftxt2xmlstr.defaultParams;
   const words = fftxt.split(/\s/g);
   if (words.length == 0) {
     return null;
   }
   else if (words[0] == "") {
     // the first word is whitespace
-    // length 2: <Role>words[1]</Role>
+    // length 2: <Role roleName="words[1]" action="params.roleAction"/>
     // length 3+: <Annotation>words[1]+</Annotation>
     if (words.length == 1) {
       return null;
     }
     else if (words.length == 2) {
-      // TODO the Role node is added as a child, but it doesn't really work as a roleName
-      return '<Role>' + words[1] + '</Role>';
+      return '<Role roleName="' + words[1] + '" action="' + params.roleAction + '"/>';
     }
     else {
       return '<Annotation>' + fftxt.trim() + '</Annotation>';
     }
   }
   else if (words.length == 1) {
-    // the simplest case
-    return '<' + words[0] + '/>';
+    if (words[0].trim().startsWith("http")) {
+      return '<Annotation>' + fftxt.trim() + '</Annotation>';
+    }
+    else {
+      return '<' + words[0] + '/>';
+    }
   }
   else if (words.length == 2) {
     return '<' + words[0] + ' roleName="' + words[1] + '"/>';
